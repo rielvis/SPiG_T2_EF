fb74bd315ac1f0bf6cc54bbefc3b81818cb27cb5
R Davis <grd116@student.aru.ac.uk>
2026-02-05 09:47:48 +0000

    Added reference to AnimatedSprite2D so to flip the sprite when it changes direction

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -4,6 +4,16 @@ const speed = 50
 
 var direction = 1
 
+@onready var ray_cast_2d_r: RayCast2D = $RayCast2D_R
+@onready var ray_cast_2d_l: RayCast2D = $RayCast2D_L
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if ray_cast_2d_r.is_colliding():
+		direction = -1
+		animated_sprite_2d.flip_h = true
+	if ray_cast_2d_l.is_colliding():
+		direction = 1
+		animated_sprite_2d.flip_h = false
 	position.x += direction * speed * delta
