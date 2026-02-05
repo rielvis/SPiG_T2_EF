e802a2dc68ebc79d8fb08dddb7f2deabddbc970b
R Davis <grd116@student.aru.ac.uk>
2026-02-05 10:52:37 +0000

    Made the sprite flip when movement direction changes

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,8 @@ const JUMP_VELOCITY = -300.0
 # Grab the gravity setting from project settings
 # var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -17,8 +19,15 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
+	
+	# Flips the sprite
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true
+	
+	# Applies movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
