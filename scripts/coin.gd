8a6b9efb0ecbd7c80f644e284450adb069838ece
R Davis <grd116@student.aru.ac.uk>
2026-02-05 12:15:53 +0000

    Played around with audio

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,5 +1,8 @@
 extends Area2D
 
+@onready var game_manager: Node = %GameManager
+
 func _on_body_entered(_body):
-	print("+1 coin")
+	#print("+1 coin")
+	game_manager.add_point()
 	queue_free()
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,6 +2,8 @@ extends Node
 
 var score = 0
 
+@onready var score_label: Label = $ScoreLabel
+
 func add_point():
 	score += 1
-	print(Score)
+	score_label.text = "You collected " + str(score) + " coins!"
