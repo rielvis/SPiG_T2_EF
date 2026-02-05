f214faa7ab08dd336d6506759c6e22995b083232
R Davis <grd116@student.aru.ac.uk>
2026-02-05 10:45:23 +0000

    Amended default script to use custom input mapping

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,29 +1,26 @@
 extends CharacterBody2D
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-
 
 const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
 
+# Grab the gravity setting from project settings
+# var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-func _physics_process(delta: float) -> void:
+func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
+		# velocity += gravity * delta
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
-		if "ui_left":
-			animated_sprite_2d.flip_h = true
-		if "ui_right":
-			animated_sprite_2d.flip_h = false
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
