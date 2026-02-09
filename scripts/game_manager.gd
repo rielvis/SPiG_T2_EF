9fb7c219d182ff3ae9fda8722b90995a48429d1b
Robin Davis <grd116@student.aru.ac.uk>
2026-02-09 09:50:04 +0000

    Attempted resolve for audio panning, need to address later

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,7 +3,7 @@ extends Node
 var score = 0
 
 @onready var score_label: Label = $ScoreLabel
-@onready var coin_sfx: AudioStreamPlayer2D = $CoinSFX
+@onready var coin_sfx: AudioStreamPlayer2D = $CoinSFX # transform is fixed, would ideally put it where the player is
 
 func add_point():
 	score += 1
