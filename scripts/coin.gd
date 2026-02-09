f3ebfa076e57b3a854ea27a7f7a485ba7d96680d
Robin Davis <grd116@student.aru.ac.uk>
2026-02-09 09:45:50 +0000

    Made the coinsfx come from a GameManager node

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -3,6 +3,6 @@ extends Area2D
 @onready var game_manager: Node = %GameManager
 
 func _on_body_entered(_body):
-	#print("+1 coin")
+	print("+1 coin")
 	game_manager.add_point()
 	queue_free()
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,7 +3,9 @@ extends Node
 var score = 0
 
 @onready var score_label: Label = $ScoreLabel
+@onready var coin_sfx: AudioStreamPlayer2D = $CoinSFX
 
 func add_point():
 	score += 1
+	coin_sfx.playing = true
 	score_label.text = "You collected " + str(score) + " coins!"
